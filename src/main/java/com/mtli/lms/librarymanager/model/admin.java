63994f9813897473dfaa8f53e9d5e1c8d68dcc64
taogege <1340420560@qq.com>
2019-11-24 22:51:49 +0800

    2.1 图书管理系统基本完成（除了前端时间显示）

--- src/main/java/com/mtli/lms/librarymanager/model/admin.java
+++ src/main/java/com/mtli/lms/librarymanager/model/admin.java
@@ -3,17 +3,17 @@ package com.mtli.lms.librarymanager.model;
 import lombok.Data;
 
 /**
- * @Description:
+ * @Description:管理员实体类
  * @Author: Mt.Li
  * @Create: 2019-10-25 09:02
  */
 @Data
-public class admin {
+public class Admin {
     private Integer a_id;
-    private String a_name;
-    private Integer a_phone;
-    private String a_email;
-    private byte[] a_photo;
-    private String a_username;
-    private String a_password;
+    private String name;
+    private String phone;
+    private String email;
+    private byte[] photo;
+    private String username;
+    private String password;
 }
